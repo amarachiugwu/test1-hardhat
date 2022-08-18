3529a5eed8eb2966736e82b036e9ee7700fd93d7
amarachiugwu <marycynthiamara2@gmail.com>
2022-08-18 01:04:25 +0100

    update3

--- contracts/TokenIn.sol
+++ contracts/TokenIn.sol
@@ -3,17 +3,11 @@ pragma solidity ^0.8.0;
 
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-    
 
 contract TokenTranfer is ERC20 {
-    
-    // uint initialSupply
-    constructor()  ERC20("Hiba Finance", "HIBA")  {
-        // _mint(msg.sender, initialSupply);
+    constructor()  ERC20("Token In", "TOI")  {}
+
+    function mint (uint amount) external {
+        _mint(msg.semder, amount);
     }
-    
-    function mint (address recipient, uint amount) external {
-        _mint(recipient, amount);
-    }
-    
 }
